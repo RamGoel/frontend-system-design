ebe409e66e33ad53fc687206e0597fdb30c718ac
Ram Goel <rgoel766@gmail.com>
2023-11-30 04:18:24 +0530

    added more components

---
+++ README.md
@@ -0,0 +1,5 @@
+## Frontend System Design 
+
+This repository contains learning material for frontend engineers, whether you are upskilling or preparing for interviews. It has so much content which can help you achieve your goals.
+
+PR's are always welcome
--- frontend-system-design/with-react/src/App.js
+++ frontend-system-design/with-react/src/App.js
@@ -1,18 +1,9 @@
-import { data } from "./components/explorer/explorer.data";
-import { Explorer } from "./components/explorer/explorer.screen";
-import { HolyGrail } from "./components/holy-grail/holy-grail.screen";
-import InfiniteScroll from "./components/infinite-scroll/infinite-scroll.component";
-import { ProgressBar } from "./components/progress-bar/progress-bar.screen";
-import { Suggestions } from "./components/typeahead-input/typeahead-input.component";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Explorer explorer={data} /> */}
-      {/* <HolyGrail /> */}
-      {/* <ProgressBar cycle={10} timeout={10000} /> */}
-      {/* <Suggestions /> */}
-      <InfiniteScroll />
+      
     </div>
   );
 }
